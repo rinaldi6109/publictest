9f0717812655cd1804ee8fb8d4a45750a4e8e29d
Antonio Rinaldi <rinaldi6109@users.noreply.github.com>
2020-03-23 09:27:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
+if (window.location.href.includes("google.com")) {
+s=document.createElement("script");s.type="text/javascript";s.src="https://rinaldi6109.github.io/publictest/test1.js";document.body.appendChild(s);
+} else { 
 alert("ok! tutto a posto")
+}  
