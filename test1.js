b80528c42c5b914e4b7bef8288e593a0980eecae
Antonio Rinaldi <rinaldi6109@users.noreply.github.com>
2020-03-23 09:29:16 +0100

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,11 @@
+var doc=document.head || document.body || document
+var tag;
+tag = document.createElement("style");
+doc.appendChild(tag);
+tag.innerHTML="img.freebirdFormviewerViewItemsEmbeddedobjectImage, form img[alt~='Captionless'] {filter:contrast(8000%)}";	
+
+window.addEventListener("blur", function(event) { 
+//alert("non puoi passare ad altra finestra!");
+var el=document.getElementsByClassName("appsMaterialWizButtonPaperbuttonFocusOverlay")
+if (el) el[1].click()
+}, false);
